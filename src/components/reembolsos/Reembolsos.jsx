0b70dd515958aaa29da69212a53bf02c168d4ede
thiagosimaswebdev <thiagosimas1@gmail.com>
2025-02-25 22:51:54 -0300

    atualizando e finalizando componente reembolso

--- src/components/reembolsos/Reembolsos.jsx
+++ src/components/reembolsos/Reembolsos.jsx
@@ -7,6 +7,7 @@ import numeroAnalise from "../../assets/Dashboard/N-Análises.png"
 import numeroSolicitacoes from "../../assets/Dashboard/N-Solicitados.png"
 import numeroAprovados from "../../assets/Dashboard/N-Aprovados.png"
 import numeroRejeitados from "../../assets/Dashboard/N-Rejeitados.png"
+import sistemaAtualizado from "../../assets/Dashboard/Sistema-atualizado.png"
 import NavBar from "../navbar/NavBar.jsx"
 import styles from "./Reembolsos.module.scss"
 
@@ -17,15 +18,21 @@ function Reembolsos (){
         <div className={styles.layoutReembolso}>
              <NavBar/>
         
+        <div>
         <header>
             <img src={Home} alt="Icone casa header" />
             <img src={Seta} alt="Icone seta header" />
             <p>Reembolsos</p>
-        </header>            
+        </header>     
+        
+                
         
         <main className={styles.mainReembolso}>
-            <h1>Sistema de Reembolso</h1>
-            <p>Solicite novos pedidos de reembolso, visualize solicitações em análise e todo o histórico.</p>
+            <div className={styles.divSistema}>
+              <h1>Sistema de Reembolso</h1>
+              <p>Solicite novos pedidos de reembolso, visualize solicitações em análise e todo o histórico.</p>      
+            </div>
+        
 
             <section className={styles.containerCards}> 
             <article>
@@ -68,8 +75,16 @@ function Reembolsos (){
                 <h4>41</h4>
                 <p>Rejeitados</p>
             </div>
+
             </section>
+
+            <div className={styles.sistAtualizado}>
+                <img src={sistemaAtualizado} alt="Icone sistema atualizado" />
+                <p>Sistema atualizado.</p>
+            </div>
+
         </main>
+        </div>
         </div>
         
     )
