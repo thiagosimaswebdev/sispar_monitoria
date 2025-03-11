063781ba1861a590cd161547df5ae25475397ef6
thiagosimaswebdev <thiagosimas1@gmail.com>
2025-03-11 20:25:56 -0300

    atualizando

--- src/App.jsx
+++ src/App.jsx
@@ -18,22 +18,4 @@ function App(){
 
 }
 
-// import { HashRouter, Routes, Route } from "react-router-dom";
-// import Login from "./components/login/Login.jsx";
-// import Reembolsos from "./components/reembolsos/Reembolsos.jsx";
-// import Solicitacao from "./components/solicitacao/Solicitacao.jsx";
-// import "./global.scss";
-
-// function App() {
-//   return (
-//     <HashRouter>
-//       <Routes>
-//         <Route path="/" element={<Login />} />
-//         <Route path="/reembolsos" element={<Reembolsos />} />
-//         <Route path="/solicitacao" element={<Solicitacao />} />
-//       </Routes>
-//     </HashRouter>
-//   );
-// }
-
 export default App
